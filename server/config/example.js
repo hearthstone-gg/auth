727ad4c1143908f8180a9a2ba9d0ac9ec0901e02
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-05-23 18:55:59 -0400

    add *

--- server/config/example.js
+++ server/config/example.js
@@ -1,7 +1,7 @@
-var config = require('hs.gg-config').get('local').services.auth;
+var config = require('hs.gg-config').get('example').services.auth;
 
 module.exports = {
-    "env": "local",
+    "env": "example",
     "server": {
         "port": config.port,
         "sslPort": config.httpsPort
@@ -9,8 +9,8 @@ module.exports = {
     "db": "mongodb://localhost/hsgg",
     "auth": {
         "bnetAuth": {
-            "clientID": "",
-            "clientSecret": "",
+            "clientID": "************",
+            "clientSecret": "************",
             "callbackURL": 'https://' + config.domain + "/auth/bnet/callback"
         }
     },
