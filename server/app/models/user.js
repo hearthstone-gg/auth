6b1389f82ec2ded1c88aba679b36c1b9960ec4ba
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-09-05 23:28:15 -0400

    swithc to user config

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -1,37 +1,11 @@
 /*
 	User Model
 */
-var uuid = require('node-uuid');
+var conf = require('hs.gg-config').get('local');
+console.log(conf)
 
 var User = exports.User = function(options, mongoose) {
 	var self = this;
-	var Schema = mongoose.Schema;
-
-	var userSchema = new Schema({
-		bnet: {
-			id: String,
-			token: String,
-			name: String
-		},
-		token: String
-	});
-
-	userSchema.methods.getDisplayName = function() {
-		if (this.bnet.name) {
-			name = this.bnet.name.split('#')[0];
-		}
-		return name.toLowerCase();
-	}
-
-	userSchema.methods.generateToken = function(cb){
-		//TODO use bnet token in hash
-		this.token = uuid.v4();
-		this.save(function() {
-			if (cb) { cb(); }
-		});
-	};
-
-	var UserModel = mongoose.model('User', userSchema);
-
-	return UserModel;
+	
+	return conf.services.models.user;
 };
