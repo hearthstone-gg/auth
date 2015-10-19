d397c7c6e315c441bddacb4a0ec6e544a6a2d771
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-10-18 23:28:27 -0400

    fix user model for auth

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -6,5 +6,5 @@ var config = require('hs.gg-config').get('local');
 
 var User = exports.User = function(options, mongoose) {
 	
-	return config.models.user(mongoose);
+	return mongoose.model('User', config.models.user(mongoose));
 };
